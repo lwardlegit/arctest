01ac15377574048941940da49db0b9e966e450a9
luther <lwardle@unca.edu>
2020-04-21 13:57:14 -0700

    finished base project

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,73 @@
 <template>
-    <div id="app">
-        <web-map v-bind:centerX="lat" v-bind:centerY="long" ref="map"/>
+  <div id="app">
+    <web-map id="mapComponent" ref="map" :coords="coords"/>
 
-        <div class="center">
-          <b-button class="btn-block" @click="getLocation" variant="primary">My Location</b-button>
-        </div>
+    <div class="center">
+      <b-button class="btn-block" @click="updateCenter()" variant="primary">My Location</b-button>
+
+      <p>after placing nodes on the map, select an analysis tool from the dropdown menu</p>
+
+      <b-dropdown>
+        <b-dropdown-item @click="getAvg('rain')">rainfall</b-dropdown-item>
+        <b-dropdown-item @click="getAvg('wind')">wind speed</b-dropdown-item>
+        <b-dropdown-item @click="getAvg('elevation')">elevation</b-dropdown-item>
+      </b-dropdown>
+
     </div>
-
-    
+  </div>
 </template>
 
 <script>
-import WebMap from './components/webmap.vue';
+import WebMap from "./components/WebMap";
 
 export default {
-    name: 'App',
-    components: { WebMap }, 
-    data(){
-      return{
-        lat: null,
-        long: null,
+  name: "App",
+  components: {
+    WebMap
+  },
+  data: () => ({
+    coords: {
+      latitude: -118,
+      longitude: 34
+    },
+  }),
+  methods: {
+    getAvg(param){
+      this.$refs.map.calcAvg(param)
+    },
+
+    updateCenter() {
+      this.getLocation();
+    },
+    getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+          this.coords = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          };
+        });
+      } else {
+        console.log("Geolocation not supported by your browser.");
       }
-    },
-    methods:{
-
-      showPos(pos){
-
-        this.lat = pos.coords.latitude
-        this.long = pos.coords.longitude
-
-        
-        console.log('new location',this.lat,this.long, this.$refs)   
-      },
-
-
-      getLocation(){
-        if (navigator.geolocation) {
-              navigator.geolocation.getCurrentPosition(this.showPos);
-              this.$refs.map.somethingElse();
-          } else { 
-            console.log("Geolocation is not supported by this browser.");
-          }
-      },
-
-    
-      
-    },
+    }
+  }
 };
 </script>
-
-<style>
-html,body {
-  margin: 0;
-  padding: 0;
-  height: 100%;
-  width: 100%;
-}
+<style lang="scss" src="bootstrap-scss/bootstrap.scss"></style>
+<style lang="scss">
 #app {
   display: flex;
+  min-height: 100vh;
+  width: 65%;
+  margin: 2em auto;
   flex-direction: column;
-  padding: 0;
-  margin: 0 auto;
-  margin-top: 5em;
-  width: 50%;
-  height: 60%;
-}
-.center{
-  margin-top: 2em;
+  > *:first-child {
+    flex-grow: 1;
+  }
+  #mapComponent{
+    border: 1px solid black;
+    margin: 3em;
+  }
 }
 </style>
--- src/components/webmap.vue
+++ src/components/webmap.vue
@@ -3,62 +3,124 @@
 </template>
 
 <script>
-import { loadModules } from 'esri-loader';
-
+import { loadModules } from "esri-loader";
+let E = {}; // placeholder for Esri modules
+// (could have used `this`, but it didn't feel right...)
 export default {
-  name: 'web-map',
-  props:['centerX', 'centerY'],
-  data: function(){
-    return{
-      X: this.centerX,
-      Y: this.centerY,
-  
-    }
+  name: "web-map",
+  props: {
+    coords: {
+      type: Object,
+      required: true
+    },
+  },
+  data: function() {
+    return {
+      view: null,
+      nodes: []
+    };
   },
   mounted() {
-    console.log('new data',this.X,this.Y)
-
     // lazy load the required ArcGIS API for JavaScript modules and CSS
-    loadModules(['esri/Map', 'esri/views/MapView'], { css: true })
-    .then(([ArcGISMap, MapView]) => {
-      const map = new ArcGISMap({
-        basemap: 'topo-vector'
+    loadModules([
+        "esri/Map", 
+        "esri/views/MapView", 
+        "esri/geometry/Point", 
+        "esri/Graphic",
+        "esri/layers/GraphicsLayer",
+        "esri/widgets/Sketch"], {
+      css: true
+    }).then(([ArcGISMap, MapView, Point, Graphic, GraphicsLayer, Sketch]) => {
+      E.ArcGISMap = ArcGISMap;
+      E.MapView = MapView;
+      E.Point = Point;
+      const map = new E.ArcGISMap({
+        basemap: "topo-vector"
       });
 
-      this.view = new MapView({
+      this.view = new E.MapView({
         container: this.$el,
         map: map,
-        center: [-118,34],   ///USE PROPS HERE FOR NEW CENTER
-        zoom: 8
+        center: [this.coords.latitude, this.coords.longitude],
+        zoom: 9
       });
+        var graphicsLayer = new GraphicsLayer();
+        // create a new sketch widget
+        const sketch = new Sketch({
+          view:this.view,
+          layer: graphicsLayer
+        });
+
+      var point = {
+         type: "point",
+         longitude: -118.80657463861,
+         latitude: 34.0005930608889
+       };
+
+       var simpleMarkerSymbol = {
+         type: "simple-marker",
+         color: [226, 119, 40],  // orange
+         outline: {
+           color: [255, 255, 255], // white
+           width: 1
+         }
+       };
+
+       var pointGraphic = new Graphic({
+         geometry: point,
+         symbol: simpleMarkerSymbol
+       });
+
+        
+
+      
+      this.view.ui.add(sketch, "top-right");
+
+      graphicsLayer.add(pointGraphic);
+       map.add(graphicsLayer);
+
+
     });
+   
+  },
+  watch: {
+    coords() {
+      this.showPos();
+    }
   },
   beforeDestroy() {
     if (this.view) {
-      // destroy the map view
       this.view.container = null;
     }
   },
-  methods:{
-    somethingElse(){
-   
-      console.log('map x',this.view)
-      console.log('props',this.X,this.Y)
-      this.view.goTo({center:[70,70]})
-    
+  methods: {
+    showPos() {
+      if (E.Point) {
+        const point = new E.Point(this.coords.longitude, this.coords.latitude);
+        this.view.goTo({ center: point });
+      } else {
+        console.log("Map modules are still loading...");
+      }
+    },
+
+    calcAvg(param){
+      console.log('calc average',param)
+
+      switch(param) {
+          case 'wind':
+            console.log('wind nodes',this.nodes)
+            break;
+          case 'rain':
+            console.log('rain nodes',this.nodes)
+            break;
+          case 'elevation':
+            console.log('elevation nodes',this.nodes)
+            break;
+          default:
+            // code block
+        }
     },
   }
-
 };
-
 </script>
 
-<style scoped>
-div {
-  padding: 0;
-  margin: 0;
-  width: 100%;
-  height: 100%;
-  border: 1px solid black;
-}
-</style>
